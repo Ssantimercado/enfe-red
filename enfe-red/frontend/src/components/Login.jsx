b3922b2363a91da15a8ca8e8ce5e1b95b3f83c3b
pabloomercadoo <mercadopablo2004@gmail.com>
2026-09-07 19:03:54 -0300

    Arreglo en Login

--- enfe-red/frontend/src/components/Login.jsx
+++ enfe-red/frontend/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // 1. Importamos el hook de navegación
 
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
